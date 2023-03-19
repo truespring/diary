4d2891823fa43ff1fb67d0c650a0b304d48746ad
truespring <truespring1@gmail.com>
2023-03-19 17:32:39 +0900

    설정 추가중

--- src/main/kotlin/com/rubok/diary/user/domain/UserType.kt
+++ src/main/kotlin/com/rubok/diary/user/domain/UserType.kt
@@ -4,11 +4,11 @@ import lombok.Getter
 
 @Getter
 class UserType {
-    private val userId: Int? = null
+    private val userId: Long? = null
     private val role: String? = null
     private val useYn: String? = null
 
-    fun toUser(userId: Int?, role: String?, useYn: String?): SecurityUser {
+    fun toUser(userId: Long?, role: String?, useYn: String?): SecurityUser {
         return SecurityUser(userId!!, role!!, useYn!!)
     }
 }
